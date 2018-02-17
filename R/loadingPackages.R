03a9380fbf75f5b10e98faa2ba1658afab17a265
Mikey Harper <mikey-harper@hotmail.com>
2018-02-17 21:18:47 +0000

    Added additional functions

---
+++ R/loadingPackages.R
@@ -0,0 +1,32 @@
+#' Installs and loads packages
+#'
+#' \code{myRequiredPackages} checks whether the package is already installed,
+#'   installing those which are not preinstalled. All the libraries are then load.
+#'
+#'   Especially useful when running on virtual machines where package installation
+#'   is not persistent (Like UoS sve). It will fail if the packages need to be
+#'   installed but there is no internet access
+#'
+#' @param packages A list of packages
+#' @param repository The repository to load functions from. Defaults to "https://cran.ma.imperial.ac.uk/"
+#'
+#' @author Luke Blunden, \email{lsb@@soton.ac.uk} (original)
+#' @author Michael Harper \email{m.harper@@soton.ac.uk} (revised version)
+#' @export
+#'
+myRequiredPackages <- function(packages, repository = "https://cran.ma.imperial.ac.uk/"){
+
+  # Find if package isn't installed
+  newPackages <- packages[!(packages %in% utils::installed.packages()[,1])]
+
+  # Install if required
+  if (length(newPackages)){install.packages(newPackages, dependencies = TRUE)}
+
+  # Load packages
+  sapply(packages, require, character.only = TRUE)
+}
+
+
+
+
+
